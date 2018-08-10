b67f01e0bb4383131b44eed81fc5d7a8cd5d2933
Matthew Pelosi <mattpelosi@gmail.com>
2018-08-10 09:02:09 -0700

    updated gitignore

--- server.js
+++ server.js
@@ -18,4 +18,4 @@ process.on("unhandledRejection", error => {
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log(`Express is listening on port ${port}`);
+console.log(`Server is listening on port ${port}`);
