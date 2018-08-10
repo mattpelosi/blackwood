0bf54091f32a0e770666ce695e8abd560225b3fc
Matthew Pelosi <mattpelosi@gmail.com>
2018-08-10 11:12:32 -0700

    issue/missing GET pat in build

--- server.js
+++ server.js
@@ -9,13 +9,19 @@ require("dotenv").config();
 app.use(bodyParser.json());
 app.use(router);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
 const url = process.env.MONGODB_URL
+console.log(url);
 
 mongo.connect(url);
 
 process.on("unhandledRejection", error => {
   console.log("unhandled rejection", error);
 });
+
 
 const port = process.env.PORT || 5000;
 app.listen(port);
